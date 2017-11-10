0bcea2d2618ea351805b41592fb2039ec081fc85
Lukasz Pater <miszobi@gmail.com>
2017-11-10 07:38:25 +0100

    initial scaffold

---
+++ src/index.js
@@ -0,0 +1,4 @@
+window.jQuery = require('jquery')
+require('@atlassian/aui/dist/aui/js/aui.js')
+require('@atlassian/aui/dist/aui/js/aui-experimental.js')
+require('@atlassian/aui/dist/aui/css/aui.css')
